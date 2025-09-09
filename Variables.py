978c07dd94137c698df38a9d5db193fba740cc68
haelkl <wigginst27@chcstudent.com>
2025-09-09 14:00:39 -0400

    commit

---
+++ Variables.py
@@ -0,0 +1,16 @@
+X = 5
+print (X)
+y = 10 
+print (X + y)
+print (X - y)
+print (X * y) 
+print (X / y) 
+print (X ** 2)
+Sum = X + y
+print(Sum)
+Sum = Sum + 20 
+print (Sum)
+name ="Ty Wiggins"
+print (name)
+print (f"My name is {name}")
+print (f"the sum of x and y is {Sum}")
