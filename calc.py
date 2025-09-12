c21863ec745dbcc7cfeb834df9ab421914a1e02a
haelkl <kloiberh27@chcstudent.com>
2025-09-12 12:30:26 -0400

    commit

---
+++ calc.py
@@ -0,0 +1,13 @@
+num1 = input ("Enter num 1:")
+num2 = input ("Enter num 2:")
+num1 = int (num1)
+num2 = int (num2)
+num2 = (num2)
+sum = num1 + num2
+print (f"The sum of {num1} and {num2} is {sum} ")
+num3 = input (" enter num 3:")
+num4 = input ("enter num 4:")
+num3 = int (num3)
+num4 = int (num4) 
+sum = num3 - num4 
+print (f"the sum of {num3} and {num4} is {sum}")
---
+++ variables2.py
@@ -0,0 +1,6 @@
+numsix = input("Enter your number")
+numsix = int(numsix)
+lorkeez = input ("Enter your second number")
+lorkeez = int (lorkeez)
+sum = numsix + lorkeez 
+print(f"the sum is {sum}")
