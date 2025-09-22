e4413d0ea98afbdb993a39e4c9fc58c2639e6cff
haelkl <kloiberh27@chcstudent.com>
2025-09-22 12:30:14 -0400

    commit

---
+++ fish.py
@@ -0,0 +1,14 @@
+option = input ("enter fish type")
+if option == "carnivorious":
+    option = input ("do you already have this fish?")
+    if option == "yes":
+        print("too bad")
+    elif option == "no":
+        print("enjoy")
+elif option == "salt water":
+    option = input
+    print("your a fancy fish parent")
+elif option == "community":
+    print("you should get more than one")
+elif:
+    print ("I dont think thats a fish")
---
+++ hailstone.py
@@ -0,0 +1,10 @@
+option = input("Enter number 1")
+option = int(option)
+option2 = input("Enter number 2")
+option2 = int(option2)
+if option > option2:
+     print ("number 1 wins")
+if option < option2:
+    print ("number 2 wins")
+if option == option2:
+    print ("numbers are equal")
